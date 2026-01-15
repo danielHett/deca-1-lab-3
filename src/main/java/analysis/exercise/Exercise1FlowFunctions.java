fc211c9702ccdb32f4e8ebb80962902d376282fc
Daniel Hettinger <danielhettinger916@gmail.com>
2026-01-15 21:20:17 +0100

    all tests passing for first exercise

--- src/main/java/analysis/exercise/Exercise1FlowFunctions.java
+++ src/main/java/analysis/exercise/Exercise1FlowFunctions.java
@@ -4,15 +4,20 @@ import analysis.TaintAnalysisFlowFunctions;
 import analysis.VulnerabilityReporter;
 import analysis.fact.DataFlowFact;
 import com.google.common.collect.Sets;
+
 import heros.FlowFunction;
+import sootup.core.jimple.basic.Immediate;
 import sootup.core.jimple.basic.Local;
 import sootup.core.jimple.basic.Value;
 import sootup.core.jimple.common.expr.AbstractInstanceInvokeExpr;
+import sootup.core.jimple.common.expr.AbstractInvokeExpr;
 import sootup.core.jimple.common.stmt.JAssignStmt;
 import sootup.core.jimple.common.stmt.Stmt;
 import sootup.core.model.SootMethod;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 
 public class Exercise1FlowFunctions extends TaintAnalysisFlowFunctions {
@@ -25,13 +30,40 @@ public class Exercise1FlowFunctions extends TaintAnalysisFlowFunctions {
 
     @Override
     public FlowFunction<DataFlowFact> getCallFlowFunction(Stmt callSite, SootMethod callee) {
+        /**
+         * Check if the fact is an argument in any of the arguments at the callSite. If
+         * so, then
+         * make sure the parameter is in the out set.
+         */
         return fact -> {
             if (fact.equals(DataFlowFact.getZeroInstance()))
                 return Collections.emptySet();
-            prettyPrint(callSite, fact);
+
             Set<DataFlowFact> out = Sets.newHashSet();
 
             // TODO: Implement Exercise 1c) here
+            if (callSite.containsInvokeExpr()) {
+                AbstractInvokeExpr invokeExpr = callSite.getInvokeExpr();
+
+                // Find all arguments in the invoke expression that are equal to the fact.
+                List<Integer> taintedArgumentIndicies = new ArrayList<Integer>();
+                for (int i = 0; i < invokeExpr.getArgCount(); i++) {
+                    Immediate localOrConstant = invokeExpr.getArg(i);
+
+                    // We only care about locals.
+                    if (!(localOrConstant instanceof Local))
+                        continue;
+
+                    // If it's tainted, we mark the index.
+                    Local local = (Local) localOrConstant;
+                    if (fact.getVariable().equals(local))
+                        taintedArgumentIndicies.add(i);
+                }
+
+                for (int i : taintedArgumentIndicies) {
+                    out.add(new DataFlowFact(callee.getBody().getParameterLocal(i)));
+                }
+            }
 
             return out;
         };
@@ -42,7 +74,7 @@ public class Exercise1FlowFunctions extends TaintAnalysisFlowFunctions {
          * The purpose of this method is to generate a flow function based on the
          * statement passed. Note that unlike the flow functions from the monotone
          * framework, we call this flow function once per element
-         * in the input set, which is why *val* is just one dataflow fact, and not an
+         * in the input set, which is why *fact* is just one dataflow fact, and not an
          * entire set.
          */
         return fact -> {
